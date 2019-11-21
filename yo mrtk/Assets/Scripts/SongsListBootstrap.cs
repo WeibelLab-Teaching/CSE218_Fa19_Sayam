f7d3f8b5e79d6d97d1ed5c08c10637bf59dddc7e
Sabrina Sampaio-simao <ax006680@UCSD.EDU>
2019-11-20 16:27:42 -0800

    New deployment branch with our new deployment test for thursday. simple menu opening a simple keyboard demo and playing. DONT MERGE WITH MASTER

---
+++ yo mrtk/Assets/Scripts/SongsListBootstrap.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class SongsListBootstrap : MonoBehaviour
+{
+    public Transform songsList;
+    public Transform songItemPrefab;
+    public Transform keyboard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i=0; i < GameState.Instance.songs.Length; i++)
+        {
+            Transform item = Object.Instantiate(songItemPrefab);
+            Debug.Log(item);
+            Debug.Log(item.Find("ButtonContent"));
+            Debug.Log(item.Find("ButtonContent").Find("Label"));
+
+            item.Find("ButtonContent").Find("Label").GetComponent<TextMesh>().text = GameState.Instance.songs[i];
+            item.parent = songsList;
+            item.position = new Vector3(songsList.position.x, songsList.position.y - 0.1f - i * 0.1f, songsList.position.z);
+
+            item.GetComponent<SongItemHandler>().songList = songsList;
+            item.GetComponent<SongItemHandler>().keyboard = keyboard;
+            item.GetComponent<SongItemHandler>().currentItem = item;
+
+            item.GetComponent<Interactable>().OnClick.AddListener(item.GetComponent<SongItemHandler>().OnAirTap);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
